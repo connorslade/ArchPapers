c7cbd514f73c7517ba75e8954ea5b08efdedb241
Connor Slade <connor@connorcode.com>
2023-07-02 18:11:54 -0400

    Update clap

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,71 @@
-use clap::{App, Arg};
+use std::path::PathBuf;
+
+use clap::{command, value_parser, Arg};
 
 const BASE: &[u8] = include_bytes!("../data/base.png");
 
 fn main() {
-    let arg = App::new("Archpapers")
-        .version("1.0")
-        .author("Connor Slade <connor@connorcode.com>")
-        .about("Genarate Arch Linux Wallpapers")
+    let arg = command!()
+        // .version(VERSION)
+        // .author("Connor Slade <connor@connorcode.com>")
+        // .about("Generate Arch Linux Wallpapers")
         .arg(
-            Arg::with_name("INPUT")
+            Arg::new("INPUT")
                 .help("Define background image to use")
                 .required(true),
         )
         .arg(
-            Arg::with_name("OUTPUT")
+            Arg::new("OUTPUT")
                 .help("Define output file to write to")
                 .required(true),
         )
         .arg(
-            Arg::with_name("blur")
+            Arg::new("blur")
                 .help("Blur the background image")
-                .short("b")
+                .short('b')
                 .long("blur")
-                .takes_value(true)
-                .validator(|x| {
-                    if x.parse::<f32>().is_err() {
-                        return Err("Must be a vaild float (f32)".to_owned());
-                    }
-                    Ok(())
-                }),
+                .value_parser(value_parser!(f32)),
         )
         .arg(
-            Arg::with_name("darken")
+            Arg::new("darken")
                 .help("Darken the background image")
-                .short("d")
+                .short('d')
                 .long("darken")
-                .takes_value(true)
-                .validator(|x| {
-                    if x.parse::<i32>().is_err() {
-                        return Err("Must be a vaild intager (i32)".to_owned());
-                    }
-                    Ok(())
-                }),
+                .value_parser(value_parser!(i32)),
         )
         .get_matches();
 
-    let inp_file = arg.value_of("INPUT").unwrap();
-    let out_file = arg.value_of("OUTPUT").unwrap();
+    let inp_file = arg.get_one::<PathBuf>("INPUT").unwrap();
+    let out_file = arg.get_one::<PathBuf>("OUTPUT").unwrap();
 
-    println!("[*] Starting");
+    println!("[*] Starting ArchPapers V{}", env!("CARGO_PKG_VERSION"));
     let base = image::load_from_memory_with_format(BASE, image::ImageFormat::Png).unwrap();
-    println!("[*] Loading `{}`", inp_file);
+    println!("[*] Loading `{}`", inp_file.to_string_lossy());
     let mut bg = match image::open(inp_file) {
         Ok(i) => i,
         Err(_) => return println!("[-] Invalid Image Input"),
     };
 
     // Blur
-    if let Some(i) = arg.value_of("blur") {
-        println!("[*] Bluring Image");
-        bg = bg.blur(i.parse().unwrap());
+    if let Some(i) = arg.get_one::<f32>("blur") {
+        println!("[*] Blurring Image");
+        bg = bg.blur(*i);
     }
 
     // Darken
-    if let Some(i) = arg.value_of("darken") {
+    if let Some(i) = arg.get_one::<i32>("darken") {
         println!("[*] Darking Image");
-        bg = bg.brighten(-i.parse::<i32>().unwrap());
+        bg = bg.brighten(-i);
     }
 
-    println!("[*] Genarateing Image");
+    println!("[*] Generating Image");
     let bg_dim = bg.clone().into_rgba8().dimensions();
     let base = base.resize_to_fill(bg_dim.0, bg_dim.1, image::imageops::Triangle);
 
     image::imageops::overlay(&mut bg, &base, 0, 0);
 
-    println!("[*] Saveing Image to `{}`", out_file);
-    match bg.save(out_file) {
-        Ok(_) => {}
-        Err(_) => return println!("[-] Invalid Image Output"),
+    println!("[*] Saving Image to `{}`", out_file.to_string_lossy());
+    if let Err(_) = bg.save(out_file) {
+        println!("[-] Invalid Image Output");
     };
 }
